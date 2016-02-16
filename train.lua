9efd95c7f4682744aa8d06eac40d56ffd20293c8
Justin Johnson <j26774@gmail.com>
2016-02-15 17:58:02 -0800

    add CPU and OpenCL support to training script, but OpenCL training is broken for now so disable it

--- train.lua
+++ train.lua
@@ -1,7 +1,5 @@
 require 'torch'
-require 'cutorch'
 require 'nn'
-require 'cunn'
 require 'optim'
 
 require 'LanguageModel'
@@ -15,6 +13,8 @@ local cmd = torch.CmdLine()
 -- Dataset options
 cmd:option('-input_h5', 'data/tiny-shakespeare.h5')
 cmd:option('-input_json', 'data/tiny-shakespeare.json')
+cmd:option('-batch_size', 50)
+cmd:option('-seq_length', 50)
 
 -- Model options
 cmd:option('-model_type', 'lstm')
@@ -23,10 +23,6 @@ cmd:option('-rnn_size', 128)
 cmd:option('-num_layers', 2)
 cmd:option('-dropout', 0)
 
--- Batch options
-cmd:option('-batch_size', 50)
-cmd:option('-seq_length', 50)
-
 -- Optimization options
 cmd:option('-max_epochs', 50)
 cmd:option('-learning_rate', 2e-3)
@@ -34,19 +30,44 @@ cmd:option('-grad_clip', 5)
 cmd:option('-lr_decay_every', 5)
 cmd:option('-lr_decay_factor', 0.5)
 
+-- Output options
 cmd:option('-print_every', 1)
 cmd:option('-checkpoint_every', 1000)
 cmd:option('-checkpoint_name', 'cv/checkpoint')
 
+-- Benchmark options
 cmd:option('-speed_benchmark', 0)
 cmd:option('-memory_benchmark', 0)
 
+-- Backend options
 cmd:option('-gpu', 0)
+cmd:option('-gpu_backend', 'cuda')
 
 local opt = cmd:parse(arg)
 
 
-cutorch.setDevice(opt.gpu + 1)
+-- Set up GPU stuff
+local dtype = 'torch.FloatTensor'
+if opt.gpu < 0 then
+  -- Memory benchmarking is only supported in CUDA mode
+  opt.memory_benchmark = 0
+  print 'Running in CPU mode'
+elseif opt.gpu >= 0 and opt.gpu_backend == 'cuda' then
+  require 'cutorch'
+  require 'cunn'
+  cutorch.setDevice(opt.gpu + 1)
+  dtype = 'torch.CudaTensor'
+  print(string.format('Running with CUDA on GPU %d', opt.gpu))
+elseif opt.gpu >= 0 and opt.gpu_backend == 'opencl' then
+  -- Disabling OpenCL support for now because nn.LookupTable is broken in clnn.
+  assert(false, 'OpenCL backend is not supported yet. Sorry!')
+  -- Memory benchmarking is only supported in CUDA mode
+  require 'cltorch'
+  require 'clnn'
+  cltorch.setDevice(opt.gpu + 1)
+  dtype = torch.Tensor():cl():type()
+  print(string.format('Running with OpenCL on GPU %d', opt.gpu))
+end
 
 
 -- Initialize the DataLoader and vocabulary
@@ -60,9 +81,9 @@ end
 -- Initialize the model and criterion
 local opt_clone = torch.deserialize(torch.serialize(opt))
 opt_clone.idx_to_token = idx_to_token
-local model = nn.LanguageModel(opt_clone):cuda()
+local model = nn.LanguageModel(opt_clone):type(dtype)
 local params, grad_params = model:getParameters()
-local crit = nn.CrossEntropyCriterion():cuda()
+local crit = nn.CrossEntropyCriterion():type(dtype)
 
 -- Set up some variables we will use below
 local N, T = opt.batch_size, opt.seq_length
@@ -73,6 +94,8 @@ local forward_backward_times = {}
 local init_memory_usage, memory_usage = nil, {}
 
 if opt.memory_benchmark == 1 then
+  -- This should only be enabled in GPU mode
+  assert(cutorch)
   cutorch.synchronize()
   local free, total = cutorch.getMemoryUsage(cutorch.getDevice())
   init_memory_usage = total - free
@@ -86,9 +109,9 @@ local function f(w)
   -- Get a minibatch and run the model forward, maybe timing it
   local timer
   local x, y = loader:nextBatch('train')
-  x, y = x:cuda(), y:cuda()
+  x, y = x:type(dtype), y:type(dtype)
   if opt.speed_benchmark == 1 then
-    cutorch.synchronize()
+    if cutorch then cutorch.synchronize() end
     timer = torch.Timer()
   end
   local scores = model:forward(x)
@@ -103,17 +126,16 @@ local function f(w)
   local grad_scores = crit:backward(scores_view, y_view):view(N, T, -1)
   model:backward(x, grad_scores)
   if timer then
-    cutorch.synchronize()
+    if cutorch then cutorch.synchronize() end
     local time = timer:time().real
     print('Forward / Backward pass took ', time)
     table.insert(forward_backward_times, time)
   end
 
-  for j = 1, 5 do collectgarbage() end
-
   -- Maybe record memory usage
   if opt.memory_benchmark == 1 then
-    cutorch.synchronize()
+    assert(cutorch)
+    if cutorch then cutorch.synchronize() end
     local free, total = cutorch.getMemoryUsage(cutorch.getDevice())
     local memory_used = total - free - init_memory_usage
     local memory_used_mb = memory_used / 1024 / 1024
@@ -170,7 +192,8 @@ for i = 1, num_iterations do
     local val_loss = 0
     for j = 1, num_val do
       local xv, yv = loader:nextBatch('val')
-      xv, yv = xv:cuda(), yv:cuda():view(N * T)
+      xv = xv:type(dtype)
+      yv = yv:type(dtype):view(N * T)
       local scores = model:forward(xv):view(N * T, -1)
       val_loss = val_loss + crit:forward(scores, yv)
     end
@@ -199,8 +222,9 @@ for i = 1, num_iterations do
     checkpoint.model = model
     local filename = string.format('%s_%d.t7', opt.checkpoint_name, i)
     torch.save(filename, checkpoint)
-    model:cuda()
+    model:type(dtype)
     params, grad_params = model:getParameters()
+    collectgarbage()
   end
 end
 
