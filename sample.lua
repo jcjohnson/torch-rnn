b98a5ddfb3baeb93e8ca84d0d7261beff4369817
Justin Johnson <j26774@gmail.com>
2016-02-15 18:03:04 -0800

    add support for OpenCL sampling

--- sample.lua
+++ sample.lua
@@ -13,6 +13,7 @@ cmd:option('-start_text', '')
 cmd:option('-sample', 1)
 cmd:option('-temperature', 1)
 cmd:option('-gpu', 0)
+cmd:option('-gpu_backend', 'cuda')
 cmd:option('-verbose', 0)
 local opt = cmd:parse(arg)
 
@@ -20,12 +21,22 @@ local opt = cmd:parse(arg)
 local checkpoint = torch.load(opt.checkpoint)
 local model = checkpoint.model
 
-if opt.gpu >= 0 then
+local msg
+if opt.gpu >= 0 and opt.gpu_backend == 'cuda' then
   require 'cutorch'
   require 'cunn'
   cutorch.setDevice(opt.gpu + 1)
   model:cuda()
+  msg = string.format('Running with CUDA on GPU %d', opt.gpu)
+elseif opt.gpu >= 0 and opt.gpu_backend == 'opencl' then
+  require 'cltorch'
+  require 'clnn'
+  model:cl()
+  msg = string.format('Running with OpenCL on GPU %d', opt.gpu)
+else
+  msg = 'Running in CPU mode'
 end
+if opt.verbose == 1 then print(msg) end
 
 model:evaluate()
 
