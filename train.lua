6990602e9a46b1ffff690e6f8df4893bbf83d4fb
Chris Cummins <chrisc.101@gmail.com>
2016-04-24 01:32:57 +0100

    Save and restore the checkpoint iteration counter
    This patch adds a training flag '-reset_iterations' which allows users
    to restore the iteration count when resuming training from a checkpoint.
    When set to '1' (the default value), the iteration count is reset 0 when
    loading from a checkpoint:
    
        $ th train.lua $OPTIONS -init_from cv/checkpoint_20.t7
        Running in CPU mode
        Initializing from 	cv/checkpoint_20.t7
        Epoch 1.00 / 50, i = 1 / 17800, loss = 3.075188
        Epoch 1.01 / 50, i = 2 / 17800, loss = 3.014080
        ...
    
    However, if set to 0, the iteration counter (and thus epoch) is loaded
    from the checkpoint:
    
        $ th train.lua $OPTIONS -init_from cv/checkpoint_20.t7 -reset_iterations 0
        Running in CPU mode
        Initializing from 	cv/checkpoint_20.t7
        Epoch 1.06 / 50, i = 21 / 17800, loss = 3.075188
        Epoch 1.06 / 50, i = 22 / 17800, loss = 3.014080
        ...

--- train.lua
+++ train.lua
@@ -18,6 +18,7 @@ cmd:option('-seq_length', 50)
 
 -- Model options
 cmd:option('-init_from', '')
+cmd:option('-reset_iterations', 1)
 cmd:option('-model_type', 'lstm')
 cmd:option('-wordvec_size', 64)
 cmd:option('-rnn_size', 128)
@@ -83,9 +84,14 @@ end
 local opt_clone = torch.deserialize(torch.serialize(opt))
 opt_clone.idx_to_token = idx_to_token
 local model = nil
+local start_i = 0
 if opt.init_from ~= '' then
   print('Initializing from ', opt.init_from)
-  model = torch.load(opt.init_from).model:type(dtype)
+  local checkpoint = torch.load(opt.init_from)
+  model = checkpoint.model:type(dtype)
+  if opt.reset_iterations == 0 then
+    start_i = checkpoint.i
+  end
 else
   model = nn.LanguageModel(opt_clone):type(dtype)
 end
@@ -162,7 +168,7 @@ local optim_config = {learningRate = opt.learning_rate}
 local num_train = loader.split_sizes['train']
 local num_iterations = opt.max_epochs * num_train
 model:training()
-for i = 1, num_iterations do
+for i = start_i + 1, num_iterations do
   local epoch = math.floor(i / num_train) + 1
 
   -- Check if we are at the end of an epoch
@@ -219,6 +225,7 @@ for i = 1, num_iterations do
       val_loss_history_it = val_loss_history_it,
       forward_backward_times = forward_backward_times,
       memory_usage = memory_usage,
+      i = i
     }
     local filename = string.format('%s_%d.json', opt.checkpoint_name, i)
     -- Make sure the output directory exists before we try to write it
